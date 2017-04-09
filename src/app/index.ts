bee880ea5546e61cd645305f41af04d5e825cacb
bill <bill2010111@gmail.com>
2017-04-09 18:33:31 +0800

    Games list image lazy load

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,6 +5,7 @@ import { MaterialModule } from '@angular/material'
 import { StoreModule } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { EffectsModule } from '@ngrx/effects'
+import { LazyLoadImageModule } from 'ng-lazyload-image'
 import { Reducers, Services, Effects } from './services'
 import { ViewComponents } from './views'
 import MainComponents from './main'
@@ -21,6 +22,7 @@ import Pipes from './pipes'
       maxAge: 5,
     }),
     MaterialModule,
+    LazyLoadImageModule,
     AppRoutingModule,
   ],
   declarations: [
