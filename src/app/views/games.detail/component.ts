8233806eee3a30759000fe659ec7b556b241227b
bill <bill2010111@gmail.com>
2017-04-22 22:19:38 +0800

    Fix game detail rxjs bug

--- src/app/views/games.detail/component.ts
+++ src/app/views/games.detail/component.ts
@@ -10,7 +10,7 @@ import { actionTypes, actionStatus, gamePlatforms, gameGenres } from '../../cons
 })
 
 class GameComponent implements OnInit {
-  game: Game
+  game: Game = null
   trophy: GameTrophy
   trophyCompleted: number
   hideLoading: boolean = false
@@ -46,46 +46,51 @@ class GameComponent implements OnInit {
   }
 
   private manageGameState(state: GamesState): void {
-    switch (state.status) {
-      case actionStatus.FETCHED:
-        this.game = state.item
-        this.hideLoading = true
-        break
-      case actionStatus.REJECTED:
-        this.hideLoading = true
-        this.error = state.error
-        break
-      default:
-        break
+    if (!this.game) {
+      switch (state.status) {
+        case actionStatus.FETCHED:
+          this.game = state.item
+          this.hideLoading = true
+          break
+        case actionStatus.REJECTED:
+          this.hideLoading = true
+          this.error = state.error
+          break
+        default:
+          break
+      }
     }
   }
 
   private manageTrophyState(state: GameTrophyState): void {
-    if (state.status === actionStatus.FETCHED) {
+    if (state.status === actionStatus.FETCHED && state.item) {
       this.trophy = state.item
       this.trophyCompleted = state.item.earned / state.item.total * 100
     }
   }
 
   ngOnInit() {
-    if (!this.game) {
-      this.activeRoute.params.forEach((params: Params) => {
+    this.activeRoute.params.forEach((params: Params) => {
+      const url = params.url
+
+      if (!this.game) {
         this.store.dispatch({
           type: actionTypes.FETCH_ITEM,
           payload: {
-            params: params.url,
+            params: url,
             state: 'games',
           },
         })
-        this.store.dispatch({
-          type: actionTypes.FETCH_GAME_TROPHY,
-          payload: {
-            params: params.url,
-            state: 'gameTrophy',
-          },
-        })
+      }
+
+      this.store.dispatch({
+        type: actionTypes.FETCH_GAME_TROPHY,
+        payload: {
+          params: url,
+          state: 'gameTrophy',
+        },
       })
-    }
+    })
   }
 
   getPlatform(): string {
