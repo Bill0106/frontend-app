4182bd2266d1b95c2cd9351d2893a3b2089db522
bill <bill2010111@gmail.com>
2017-05-07 15:01:49 +0800

    Hearthstone matches component

---
+++ src/app/components/hearthstone-matches/component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+  selector: 'hearthstone-matches',
+  templateUrl: './template',
+})
+
+class HearthstoneMatchesComponent {}
+
+export default HearthstoneMatchesComponent
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,7 +1,9 @@
 import LazyImageComponent from './lazy-image'
 import PageStatusComponent from './page-status'
+import HearthstoneMatchesComponent from './hearthstone-matches'
 
 export default [
   LazyImageComponent,
   PageStatusComponent,
+  HearthstoneMatchesComponent,
 ]
