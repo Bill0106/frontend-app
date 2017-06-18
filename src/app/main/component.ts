902d21bd074cc5cc4ae6a7316c17cfc5debffca9
bill <bill2010111@gmail.com>
2017-06-18 16:34:59 +0800

    Google Analytics

--- src/app/main/component.ts
+++ src/app/main/component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core'
+import { Router, NavigationEnd } from '@angular/router'
+
+declare const ga: Function
 
 @Component({
   selector: 'my-app',
   templateUrl: './template',
 })
 
-class MainComponent { }
+class MainComponent {
+  constructor(router: Router) {
+    router.events.distinctUntilChanged((previous: any, current: any) => {
+      if (current instanceof NavigationEnd) {
+        return previous.url === current.url
+      }
+      return true
+    }).subscribe((x: any) => {
+      if (typeof ga !== 'undefined') {
+        ga('set', 'page', x.url)
+        ga('send', 'pageview')
+      }
+    })
+  }
+}
 
 export default MainComponent
