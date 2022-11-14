d3496a2a3f4fd6e49466ac227659ec9ff89dd9d0
Bill <bill2010111@gmail.com>
2022-11-14 17:15:33 +0800

    feat: remove emotion

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: './src',
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin'],
-    }
-  })],
+  plugins: [react()],
   resolve: {
     alias: [
       { find: '@/', replacement: '/' }
