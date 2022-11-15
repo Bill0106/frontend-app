586e16d86c11dbc23e8da07fc1ee55ceae227de0
Bill <bill2010111@gmail.com>
2022-11-15 10:45:55 +0800

    feat: split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: './src',
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   resolve: {
     alias: [
       { find: '@/', replacement: '/' }
